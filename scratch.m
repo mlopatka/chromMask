bf6ca13df25806c9fabf35ad5332e26d3e9e4023
doomling <martin.lopatka@gmail.com>
2016-02-04 21:55:13 -0700

    added deuterated anchor finding

--- scratch.m
+++ scratch.m
@@ -1,7 +1,16 @@
+addpath ../../lisca/src/
 a = dir('/Users/Martin/SURFdrive/GCxGC-MS_fireDebris/*.cdf');
+b = dir('/Users/Martin/SURFdrive/GCxGC-MS_fireDebris/*.mat');
+t = regexp([b(:).name], '.mat','split');
+t(cellfun(@isempty, t)) = [];
+
 for i = 1:numel(a)
-    eval(['c = parse_UofA_CDF(','''/Users/Martin/SURFdrive/GCxGC-MS_fireDebris/',a(i).name,''');']);
-    c.p = maskedPeakDetect(c);
-    clearvars -except a i c
-    eval(['save(','''/Users/Martin/SURFdrive/GCxGC-MS_fireDebris/',a(i).name(1:end-3),'mat'',','''-v7.3'',','''c'');']);
+    if ~ismember(a(i).name(1:end-4),t)
+        eval(['c = parse_UofA_CDF(','''/Users/Martin/SURFdrive/GCxGC-MS_fireDebris/',a(i).name,''');']);
+        c.p = maskedPeakDetect(c);
+        clearvars -except a i c
+        eval(['save(','''/Users/Martin/SURFdrive/GCxGC-MS_fireDebris/',a(i).name(1:end-3),'mat'',','''-v7.3'',','''c'');']);
+    else
+        disp('sample already processed... moving on.');
+    end
 end
